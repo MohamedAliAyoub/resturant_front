7b864d56ca7f26bcb26cdeaaa7480690ee2fab82
MohamedAliAyoub <mohamedali163163@gmail.com>
2023-12-31 23:19:32 +0200

    create category and order service

---
+++ src/app/service/order-service.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { OrderServiceService } from './order-service.service';
+
+describe('OrderServiceService', () => {
+  let service: OrderServiceService;
+
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(OrderServiceService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
