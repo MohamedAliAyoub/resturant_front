061a00b916d472c67602f175cae5a77931a91eca
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-01-01 21:07:19 +0200

    make logo dynamic

--- src/app/model/category.ts
+++ src/app/model/category.ts
@@ -1,12 +1,14 @@
 export class Category {
-  id : Number;
-  name : String;
-  date_create : Date;
-  date_update : Date;
+  id: Number;
+  name: String;
+  logo: String;
+  date_create: Date;
+  date_update: Date;
 
-  constructor(id: Number, name: String, date_create: Date, date_update: Date) {
+  constructor(id: Number, name: String, logo: string, date_create: Date, date_update: Date) {
     this.id = id;
     this.name = name;
+    this.logo = logo;
     this.date_create = date_create;
     this.date_update = date_update;
   }
