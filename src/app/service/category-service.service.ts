9211f07da16365434081ef0c7bdc53126a049921
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-01-01 04:36:30 +0200

    add service component

--- src/app/service/category-service.service.ts
+++ src/app/service/category-service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {Category} from "../model/category";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryServiceService {
 
-  constructor() { }
+  baseUrl = 'http://localhost:8080/api/allCategories';
+  constructor(private http: HttpClient) { }
+  getAllCategories():Observable<Category[]>
+  {
+    return this.http.get<Category[]>(this.baseUrl).pipe(
+      map(
+        response => response
+      )
+    )
+  }
 }
