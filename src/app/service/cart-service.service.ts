e5bac5c6eed67513b1efbf042d96fe0a4da59310
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-01-07 22:55:22 +0200

    Calculate (order Size & Orders Price)

--- src/app/service/cart-service.service.ts
+++ src/app/service/cart-service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {CartOrder} from "../model/cart-order";
 
 @Injectable({
@@ -9,14 +9,15 @@ export class CartServiceService {
   orders: CartOrder[] = [];
   totalOrders: number = 0;
   totalPrice: number = 0;
-  constructor() { }
 
-  addOrderToCart(order:CartOrder)
-  {
+  constructor() {
+  }
+
+  addOrderToCart(order: CartOrder) {
 
     let isExist: boolean = false;
-    let existOrder :CartOrder ;
-    if(this.orders.length > 0){
+    let existOrder: CartOrder;
+    if (this.orders.length > 0) {
       // for(let temp of this.orders){
       //   if(temp.id === order.id){
       //     existOrder = temp;
@@ -28,13 +29,27 @@ export class CartServiceService {
     }
     // @ts-ignore
     isExist = (existOrder != undefined); // true   false
-    if(isExist){
+    if (isExist) {
       // @ts-ignore
       existOrder.quantity++;
-    }else {
+    } else {
       this.orders.push(order)
     }
+    this.calculateTotals();
     console.log(this.orders)
+  }
+
+  calculateTotals() {
+    let totalElementsSizeOrders: number = 0;
+    let totalPriceOrders: number = 0;
+    for (let temp of this.orders){
+      totalElementsSizeOrders += temp.quantity;
+      totalPriceOrders += temp.quantity * temp.price ;
+    }
+    this.totalOrders = totalElementsSizeOrders;
+    this.totalPrice = totalPriceOrders;
+    console.log("size" +this.totalOrders);
+    console.log("price" +this.totalPrice);
   }
 
 }
