609fb9a293a2a20fbe1f8b1411f3fa809a66fc16
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-01-08 22:38:14 +0200

    assign data from formCard to toCard

--- src/app/component/check-out/check-out.component.ts
+++ src/app/component/check-out/check-out.component.ts
@@ -39,24 +39,21 @@ export class CheckOutComponent {
   }
 
   done() {
-    const fullName = this.checkoutParentGroup.get('data.fullName');
-    const fromCountry = this.checkoutParentGroup.get('fromPerson.country');
-    const toCountry = this.checkoutParentGroup.get('toPerson.country');
-    const creditCardCode = this.checkoutParentGroup.get('creditCard.code');
-
-    if (fullName && fromCountry && toCountry && creditCardCode) {
-      console.log(fullName.value);
-      console.log(fromCountry.value);
-      console.log(toCountry.value);
-      console.log(creditCardCode.value);
-    }
+    // @ts-ignore
+    console.log(this.checkoutParentGroup.get('data').value)
+    // @ts-ignore
+    console.log(this.checkoutParentGroup.get('formPerson').value)
+    // @ts-ignore
+    console.log(this.checkoutParentGroup.get('toPerson').value)
+    // @ts-ignore
+    console.log(this.checkoutParentGroup.get('creditCard.code').value)
   }
 
 
   similarGroup(event: Event) {
     if((<HTMLInputElement>event.target).checked){
       this.checkoutParentGroup.controls['toPerson']
-        .setValue(this.checkoutParentGroup.controls['fromPerson'].value)
+        .setValue(this.checkoutParentGroup.controls['formPerson'].value)
     } else {
       this.checkoutParentGroup.controls['toPerson'].reset()
     }
