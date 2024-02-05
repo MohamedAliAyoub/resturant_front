089b28f30142585c57be2dc2c86ccc11b60e1820
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-02-05 14:35:15 +0200

    login build form group

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  checkoutParentGroup!: FormGroup;
 
+  constructor(private formChildGroup: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.myFormLogin();
+  }
+
+  myFormLogin() {
+    this.checkoutParentGroup = this.formChildGroup.group({
+      user: this.formChildGroup.group({
+        email: [''],
+        password: ['']
+      })
+    });
+  }
+
+  login() {
+    alert(this.checkoutParentGroup.controls['user'].value.email);
+    alert(this.checkoutParentGroup.controls['user'].value.password);
+  }
 }
